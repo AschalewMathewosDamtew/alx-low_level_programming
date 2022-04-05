6c6679e03239c890ddc878b3a77e96adec4a7360
AschalewMathewos <aschalewthecoder@gmail.com>
2022-04-05 11:34:31 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,34 +1,69 @@
-#include "holberton.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include "main.h"
+/**
+ *_strlen - count array
+ *@s: array of elements
+ *Return: 1
+ */
+
+int _strlen(char *s)
+{
+unsigned int i;
+
+i = 0;
+while (s[i] != '\0') /*count character of string*/
+{
+i++;
+}
+
+return (i);
+}
 
 /**
- *_strdup - returns a pointer to a newly allocated space in memory
- *	which contains a copy of the string given as a parameter.
- * @str: string
- * Return: 0
+ *_strcpy - copy arrays
+ *@src: array of elements
+ *@dest: dest array
+ *Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+
+return (dest);
+}
+
+/**
+ *_strdup - array for prints a string
+ *@str: array of elements
+ *Return: pointer
  */
 
 char *_strdup(char *str)
 {
-	int i = 0, size = 0;
-	char *m;
+char *dst;
+unsigned int size;
 
-	if (str == NULL)
-		return (NULL);
+if (str == 0)
+{
+return (NULL);
+}
 
-	for (; str[size] != '\0'; size++)
-		;
+size = _strlen(str) + 1;
 
-	/*+1 on the size puts the end of string character*/
-	m = malloc(size * sizeof(*str) + 1);
+dst = (char *) malloc(size *sizeof(char));
 
-	if (m == 0)
-		return (NULL);
-	{
-		for (; i < size; i++)
-			m[i] = str[i];
-	}
-	return (m);
+if (dst == 0)
+{
+return (NULL);
+}
+_strcpy(dst, str);
+return (dst);
 }
 
